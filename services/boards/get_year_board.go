d2f9a1c873570d7f8fde4add4c06a6dd8b6a852c
Lucas Chen <lucas.chen@car8.com>
2021-04-11 18:20:52 +0800

    fix year board

--- services/boards/get_year_board.go
+++ services/boards/get_year_board.go
@@ -31,21 +31,12 @@ func (h handler) GetYearBoard(c *gin.Context) {
 		return
 	}
 
-	tianBoard, err := ziwei.NewTianBoard(birthday, gender)
-	if err != nil {
-		handleError(c, err)
-		return
-	}
-	if tianBoard, err = tianBoard.CreateTianBoard(); err != nil {
-		handleError(c, err)
-		return
-	}
-	resp := mergeTianBoardAndYearBoard(tianBoard, yearBoard)
+	resp := convertToYearBoard(yearBoard)
 
 	c.JSON(http.StatusOK, resp)
 }
 
-func mergeTianBoardAndYearBoard(tianBoard *ziwei.TianBoard, yearBoard *ziwei.YearBoard) *GetYearBoardResponse {
+func convertToYearBoard(yearBoard *ziwei.YearBoard) *GetYearBoardResponse {
 	month := toChineseNums(int(yearBoard.LunaBirthday.Month))
 	if yearBoard.LunaBirthday.IsLeap {
 		month = "閏" + month
@@ -83,7 +74,7 @@ func mergeTianBoardAndYearBoard(tianBoard *ziwei.TianBoard, yearBoard *ziwei.Yea
 			Type: TypeYearBoard,
 		})
 		board.Blocks[i].TenYearsRound = yearBlock.TenYearsRound
-		for i, star := range yearBlock.Stars {
+		for _, star := range yearBlock.Stars {
 			if board.Blocks[i] == nil {
 				board.Blocks[i] = new(Block)
 			}
@@ -96,33 +87,6 @@ func mergeTianBoardAndYearBoard(tianBoard *ziwei.TianBoard, yearBoard *ziwei.Yea
 				MiaoXian:  star.MiaoXian,
 				FourStar:  star.FourStar,
 				BoardType: TypeYearBoard,
-			})
-		}
-	}
-	for i, tianBlock := range tianBoard.Blocks {
-		if board.Blocks[i] == nil {
-			board.Blocks[i] = new(Block)
-		}
-		if len(board.Blocks[i].GongWei) == 0 {
-			board.Blocks[i].GongWei = make([]*GongWei, 0)
-		}
-		board.Blocks[i].GongWei = append(board.Blocks[i].GongWei, &GongWei{
-			Name: tianBlock.GongWeiName,
-			Type: TypeTianBoard,
-		})
-		for _, star := range tianBlock.Stars {
-			if board.Blocks[i] == nil {
-				board.Blocks[i] = new(Block)
-			}
-			if len(board.Blocks[i].Stars) == 0 {
-				board.Blocks[i].Stars = []*Star{}
-			}
-			board.Blocks[i].Stars = append(board.Blocks[i].Stars, &Star{
-				Name:      star.Name,
-				StarType:  star.StarType,
-				MiaoXian:  star.MiaoXian,
-				FourStar:  star.FourStar,
-				BoardType: TypeTianBoard,
 			})
 		}
 	}
