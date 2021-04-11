3250f5329f25be195bcf6cc2ee1c40d22a7b0fb1
Lucas Chen <lucas.chen@car8.com>
2021-04-11 18:42:48 +0800

    fix ten years board

--- services/boards/get_ten_year_board.go
+++ services/boards/get_ten_year_board.go
@@ -31,41 +31,18 @@ func (h handler) GetTenYearsBoard(c *gin.Context) {
 		return
 	}
 
-	tianBoard, err := ziwei.NewTianBoard(birthday, gender)
-	if err != nil {
-		handleError(c, err)
-		return
-	}
-	if tianBoard, err = tianBoard.CreateTianBoard(); err != nil {
-		handleError(c, err)
-		return
-	}
-	resp := mergeTianBoardAndTenBoard(tianBoard, tenBoard)
+	resp := convertToTenYearBoardResponse(tenBoard)
 
 	c.JSON(http.StatusOK, resp)
 }
 
-func mergeTianBoardAndTenBoard(tianBoard *ziwei.TianBoard, tenYearBoard *ziwei.TenYearBoard) *GetYearBoardResponse {
+func convertToTenYearBoardResponse(tenYearBoard *ziwei.TenYearBoard) *GetYearBoardResponse {
 	month := toChineseNums(int(tenYearBoard.LunaBirthday.Month))
 	if tenYearBoard.LunaBirthday.IsLeap {
 		month = "閏" + month
 	}
 	board := &GetYearBoardResponse{
-		Birthday: fmt.Sprintf("%d年%d月%d日%d時", tianBoard.Birthday.Year(), tianBoard.Birthday.Month(), tianBoard.Birthday.Day(), tianBoard.Birthday.Hour()),
-		LunaBirthDay: fmt.Sprintf("%s%s年%s月%s日%s時",
-			tianBoard.LunaBirthday.Year.TianGan.String(),
-			tianBoard.LunaBirthday.Year.DiZhi.String(),
-			month,
-			toChineseNums(int(tianBoard.LunaBirthday.Day)),
-			tianBoard.LunaBirthday.Hour,
-		),
-		Blocks:           make([]*Block, defaultBoardBlock),
-		Gender:           tianBoard.Gender.String(),
-		MingJu:           tianBoard.MingJu.JuType.String(),
-		MingJuValue:      int(tianBoard.MingJu.Number),
-		ShenZhu:          tianBoard.ShenZhu,
-		MingZhu:          tianBoard.MingZhu,
-		ShenGongLocation: tianBoard.ShenGongLocation,
+		Blocks: make([]*Block, defaultBoardBlock),
 	}
 	for i, yearBlock := range tenYearBoard.Blocks {
 		if board.Blocks[i] == nil {
@@ -83,7 +60,7 @@ func mergeTianBoardAndTenBoard(tianBoard *ziwei.TianBoard, tenYearBoard *ziwei.T
 			Type: TypeTenBoard,
 		})
 		board.Blocks[i].TenYearsRound = yearBlock.TenYearsRound
-		for i, star := range yearBlock.Stars {
+		for _, star := range yearBlock.Stars {
 			if board.Blocks[i] == nil {
 				board.Blocks[i] = new(Block)
 			}
@@ -96,33 +73,6 @@ func mergeTianBoardAndTenBoard(tianBoard *ziwei.TianBoard, tenYearBoard *ziwei.T
 				MiaoXian:  star.MiaoXian,
 				FourStar:  star.FourStar,
 				BoardType: TypeTenBoard,
-			})
-		}
-	}
-	for i, tianBlock := range tianBoard.Blocks {
-		if board.Blocks[i] == nil {
-			board.Blocks[i] = new(Block)
-		}
-		if len(board.Blocks[i].GongWei) == 0 {
-			board.Blocks[i].GongWei = make([]*GongWei, 0)
-		}
-		board.Blocks[i].GongWei = append(board.Blocks[i].GongWei, &GongWei{
-			Name: tianBlock.GongWeiName,
-			Type: TypeTianBoard,
-		})
-		for _, star := range tianBlock.Stars {
-			if board.Blocks[i] == nil {
-				board.Blocks[i] = new(Block)
-			}
-			if len(board.Blocks[i].Stars) == 0 {
-				board.Blocks[i].Stars = []*Star{}
-			}
-			board.Blocks[i].Stars = append(board.Blocks[i].Stars, &Star{
-				Name:      star.Name,
-				StarType:  star.StarType,
-				MiaoXian:  star.MiaoXian,
-				FourStar:  star.FourStar,
-				BoardType: TypeTianBoard,
 			})
 		}
 	}
